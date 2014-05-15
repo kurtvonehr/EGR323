336892814b435a7a8b9df93ce5804ac7d2695483
unknown <vonehrk@KEN244-28.egr.gvsu.edu>
2014-05-15 12:18:04 -0400

    1.7 and figures

--- Lab02/lab02.m
+++ Lab02/lab02.m
@@ -5,13 +5,15 @@
 % Lab 2
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+close all
 
 %**************************************************************
 
 % 1.6 Part A
 
 %**************************************************************
+
+figure();
 
 %Create symbolic continuous-time sinusoid
 x = sym('sin(2*pi*t/T)');
@@ -21,8 +23,10 @@ x = sym('sin(2*pi*t/T)');
 x5 = subs(x,'T',5);
 
 %plot the continuous time time function
-figure();
 ezplot(x5);
+
+%Export JPG to working directory
+hgexport(gcf, 'Lab02/fig1.jpg', hgexport('factorystyle'), 'Format', 'jpeg');
 
 clearvars
 
@@ -60,6 +64,170 @@ x16 = subs(x,'T',16);
 
 %plot the continuous time time function
 ezplot(x16);
+
+%Export JPG to working directory
+hgexport(gcf, 'Lab02/fig2.jpg', hgexport('factorystyle'), 'Format', 'jpeg');
+
+clearvars
+
+%**************************************************************
+
+% 1.6 Part C
+
+%**************************************************************
+
+figure();
+
+%create symbolic symbol for a exponential signal
+x = sym('exp(-A*t)*cos(2*pi*t)');
+%create symbolic symbol for a exponential signal
+x2 = sym('.1');
+
+%alpha = 1/2
+subplot(3,1,1);
+a1 = subs(x,'A',1/2);
+
+%plot the continuous time time function
+ezplot(a1, [0,10]);
+hold on
+ezplot(x2, [0,10]);
+
+intersect = solve(a1 == .1);
+intersect
+
+%alpha = 1/4
+subplot(3,1,2);
+a2 = subs(x,'A',1/4);
+
+%plot the continuous time time function
+ezplot(a2, [0,10]);
+hold on
+ezplot(x2, [0,10]);
+
+intersect = solve(a2 == .1);
+intersect
+
+%alpha = 1/5
+subplot(3,1,3);
+a3 = subs(x,'A',1/8);
+
+%plot the continuous time time function
+ezplot(a3, [0,20]);
+hold on
+ezplot(x2, [0,25]);
+
+intersect = solve(a3 == .1);
+intersect
+
+%Export JPG to working directory
+hgexport(gcf, 'Lab02/fig3.jpg', hgexport('factorystyle'), 'Format', 'jpeg');
+
+clearvars
+
+%**************************************************************
+%**************************************************************
+
+% 1.7 Part A
+
+%**************************************************************
+
+
+figure();
+
+%Create symbolic ramp function using heaviside function
+f = sym('t*(Heaviside(t)-Heaviside(t-2))');
+
+%plot the continuous time time function
+ezplot(f);
+
+%Export JPG to working directory
+hgexport(gcf, 'Lab02/fig4.jpg', hgexport('factorystyle'), 'Format', 'jpeg');
+
+clearvars
+
+%**************************************************************
+%**************************************************************
+
+% 1.7 Part C
+
+%**************************************************************
+
+figure();
+
+%Create symbolic ramp function using heaviside function
+f = sym('t*(Heaviside(t)-Heaviside(t-2))');
+
+%Set fundamental period of x
+%function x, variable T = 4
+subplot(5,1,1);
+g1 = subs(f,'t','-t');
+
+%plot the continuous time time function
+ezplot(g1);
+
+%Set fundamental period of x
+%function x, variable T = 8
+subplot(5,1,2);
+g2 = subs(f,'t','t+1');
+
+%plot the continuous time time function
+ezplot(g2);
+
+%Set fundamental period of x
+%function x, variable T = 16
+subplot(5,1,3);
+g3 = subs(f,'t','t-3');
+
+%plot the continuous time time function
+ezplot(g3);
+
+%Set fundamental period of x
+%function x, variable T = 8
+subplot(5,1,4);
+g4 = subs(f,'t','-t+1');
+
+%plot the continuous time time function
+ezplot(g4);
+
+%Set fundamental period of x
+%function x, variable T = 16
+subplot(5,1,5);
+g5 = subs(f,'t','-2*t+1');
+
+%plot the continuous time time function
+ezplot(g5);
+
+%Export JPG to working directory
+hgexport(gcf, 'Lab02/fig5.jpg', hgexport('factorystyle'), 'Format', 'jpeg');
+
+clearvars
+
+%**************************************************************
+%**************************************************************
+
+% 1.8 Part A
+
+%**************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
